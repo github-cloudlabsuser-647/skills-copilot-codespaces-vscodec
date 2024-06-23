134a9ca15b42bf2f8ebd9a5f3eea8778fe88eb83
github-cloudlabsuser-647 <167424129+github-cloudlabsuser-647@users.noreply.github.com>
2024-06-23 11:00:23 +0000

    Copilot third commit

---
+++ powershell.ps1
@@ -0,0 +1,13 @@
+c# Define parameters
+$resourceGroupName = "example-resources"
+$location = "West Europe"
+$storageAccountName = "examplestorageacc"
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName "Standard_LRS" # LRS = Locally redundant storage
